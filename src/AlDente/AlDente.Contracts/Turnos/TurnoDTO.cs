49902104bb4fb8f53c9a57a96b8c29c18be99b49
Martin <Martin@DESKTOP-UHEM7I1>
2021-08-07 14:53:47 -0300

    Administrar Turnos

---
+++ src/AlDente/AlDente.Contracts/Turnos/TurnoDTO.cs
@@ -0,0 +1,29 @@
+﻿using AlDente.Globalization;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlDente.Contracts.Turnos
+{
+    public class TurnoDTO
+    {
+        public int Id { get; set; }
+        public DateTime HoraInicio { get; set; }
+        public DateTime HoraFin { get; set; }
+    }
+    public class TurnoDTOValidator : AbstractValidator<TurnoDTO>
+    {
+        public TurnoDTOValidator()
+        {
+            RuleFor(x => x.HoraInicio)
+               .NotEmpty()
+                .WithMessage(Strings.XIsRequired("HoraInicio"))
+                .WithName("HoraInicio");
+            RuleFor(x => x.HoraFin)
+              .NotEmpty()
+               .WithMessage(Strings.XIsRequired("HoraFin"))
+               .WithName("HoraFin");
+        }
+    }
+}
---
+++ src/AlDente/AlDente.DataAccess/Turnos/ITurnoRepository.cs
@@ -0,0 +1,12 @@
+﻿using AlDente.DataAccess.Core;
+using AlDente.Entities.Turnos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlDente.DataAccess.Turnos
+{
+    public interface ITurnoRepository : IRepository<Turno>
+    {
+    }
+}
