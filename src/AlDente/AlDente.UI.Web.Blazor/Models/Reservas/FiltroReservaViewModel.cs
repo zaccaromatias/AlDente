e08a0e690ab30e6547dd2d2dc86c146db22cfd7c
Martin <Martin@DESKTOP-UHEM7I1>
2021-09-14 18:31:14 -0300

    Filtro por código de reserva en empleado.

---
+++ src/AlDente/AlDente.UI.Web.Blazor/Models/Reservas/FiltroReservaViewModel.cs
@@ -0,0 +1,19 @@
+﻿using AlDente.Contracts.Core;
+using AlDente.Contracts.Reservas;
+using AlDente.UI.Web.Blazor.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AlDente.UI.Web.Blazor.Models.Reservas
+{
+    public class FiltroReservaViewModel
+    {
+        private IReservaService _reservaService;
+        public List<ReservaBasicDTO> Reservas { get; private set; }
+        public async Task Buscar(string codigoFiltro)
+        {
+            var reservas = await _reservaService.GetReservaFiltroCodigo(codigoFiltro);
+            this.Reservas = new List<ReservaBasicDTO>(reservas);
+        }
+    }
+}
